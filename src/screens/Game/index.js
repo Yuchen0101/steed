b01056ff63898e6c5d78f1ecd9614d377eef777a
xiaogangy <yangxiaogang2011@gmail.com>
2021-06-11 01:25:48 +0800

    Game screen

--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -1,96 +1,97 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Dimensions, View } from "react-native";
 import { Text, Button } from "react-native-elements";
+import Carousel from "react-native-snap-carousel";
 import AppStyles from "../../AppStyles";
 import ScreenContainer from "../../components/ScreenContainer";
-import Carousel from "react-native-snap-carousel";
+import { HouseContext, useHouseContext } from './houseContext';
 import PropertyCard from "./PropertyCard";
 
-const CustomCarousel = () => {
-  const exampleItems = [
-    {
-      propertyType: "House",
-      bathrooms: 2,
-      bedrooms: 4,
-      carSpaces: 6,
-      address: "3 Castlebar Road, Lockleys SA 5032",
-      short_address: "Lockleys SA 5032",
-      sold_date: "07/07/2020",
-      short_sold_date: "May 2020",
-      areaSize: 861,
-      landsize: 570,
-      headline: "Magnificent Estate Metres from the Beach",
-      summaryDescription:
-        "On approximately 800 sqm, this welcoming period home, beautifully maintained and set in mature gardens, offers relaxed family living in a tightly-held precinct close to the Yarra, walking paths and primary and secondary schools.",
-      min_price: 1000000,
-      max_price: 1200000,
-      photos: [
-        {
-          imageType: "Property",
-          advertId: 2016914242,
-          date: "2021-04-21T00:35:22.05Z",
-          fullUrl:
-            "https://bucket-api.domain.com.au/v1/bucket/image/2016914242_1_1_210406_051501-w3901-h2600",
-          rank: 1,
-        },
-      ],
-    },
-    {
-      propertyType: "House",
-      bathrooms: 2,
-      bedrooms: 4,
-      carSpaces: 6,
-      address: "3 Castlebar Road, Lockleys SA 5032",
-      short_address: "Lockleys SA 5032",
-      sold_date: "07/07/2020",
-      short_sold_date: "May 2020",
-      areaSize: 861,
-      landsize: 570,
-      headline: "Magnificent Estate Metres from the Beach",
-      summaryDescription:
-        "On approximately 800 sqm, this welcoming period home, beautifully maintained and set in mature gardens, offers relaxed family living in a tightly-held precinct close to the Yarra, walking paths and primary and secondary schools.",
-      min_price: 1000000,
-      max_price: 1200000,
-      photos: [
-        {
-          imageType: "Property",
-          advertId: 2016914242,
-          date: "2021-04-21T00:35:22.05Z",
-          fullUrl:
-            "https://bucket-api.domain.com.au/v1/bucket/image/2016914242_1_1_210406_051501-w3901-h2600",
-          rank: 1,
-        },
-      ],
-    },
-    {
-      propertyType: "House",
-      bathrooms: 2,
-      bedrooms: 4,
-      carSpaces: 6,
-      address: "3 Castlebar Road, Lockleys SA 5032",
-      short_address: "Lockleys SA 5032",
-      sold_date: "07/07/2020",
-      short_sold_date: "May 2020",
-      areaSize: 861,
-      landsize: 570,
-      headline: "Magnificent Estate Metres from the Beach",
-      summaryDescription:
-        "On approximately 800 sqm, this welcoming period home, beautifully maintained and set in mature gardens, offers relaxed family living in a tightly-held precinct close to the Yarra, walking paths and primary and secondary schools.",
-      min_price: 1000000,
-      max_price: 1200000,
-      photos: [
-        {
-          imageType: "Property",
-          advertId: 2016914242,
-          date: "2021-04-21T00:35:22.05Z",
-          fullUrl:
-            "https://bucket-api.domain.com.au/v1/bucket/image/2016914242_1_1_210406_051501-w3901-h2600",
-          rank: 1,
-        },
-      ],
-    },
-  ];
+export const exampleItems = [
+  {
+    propertyType: "House",
+    bathrooms: 2,
+    bedrooms: 4,
+    carSpaces: 6,
+    address: "3 Castlebar Road, Lockleys SA 5032",
+    short_address: "Lockleys SA 5032",
+    sold_date: "07/07/2020",
+    short_sold_date: "May 2020",
+    areaSize: 861,
+    landsize: 570,
+    headline: "Magnificent Estate Metres from the Beach",
+    summaryDescription:
+      "On approximately 800 sqm, this welcoming period home, beautifully maintained and set in mature gardens, offers relaxed family living in a tightly-held precinct close to the Yarra, walking paths and primary and secondary schools.",
+    min_price: 1000000,
+    max_price: 1200000,
+    photos: [
+      {
+        imageType: "Property",
+        advertId: 2016914242,
+        date: "2021-04-21T00:35:22.05Z",
+        fullUrl:
+          "https://bucket-api.domain.com.au/v1/bucket/image/2016914242_1_1_210406_051501-w3901-h2600",
+        rank: 1,
+      },
+    ],
+  },
+  {
+    propertyType: "House",
+    bathrooms: 2,
+    bedrooms: 4,
+    carSpaces: 6,
+    address: "3 Castlebar Road, Lockleys SA 5032",
+    short_address: "Lockleys SA 5032",
+    sold_date: "07/07/2020",
+    short_sold_date: "May 2020",
+    areaSize: 861,
+    landsize: 570,
+    headline: "Magnificent Estate Metres from the Beach",
+    summaryDescription:
+      "On approximately 800 sqm, this welcoming period home, beautifully maintained and set in mature gardens, offers relaxed family living in a tightly-held precinct close to the Yarra, walking paths and primary and secondary schools.",
+    min_price: 1000000,
+    max_price: 1200000,
+    photos: [
+      {
+        imageType: "Property",
+        advertId: 2016914242,
+        date: "2021-04-21T00:35:22.05Z",
+        fullUrl:
+          "https://bucket-api.domain.com.au/v1/bucket/image/2016914242_1_1_210406_051501-w3901-h2600",
+        rank: 1,
+      },
+    ],
+  },
+  {
+    propertyType: "House",
+    bathrooms: 2,
+    bedrooms: 4,
+    carSpaces: 6,
+    address: "3 Castlebar Road, Lockleys SA 5032",
+    short_address: "Lockleys SA 5032",
+    sold_date: "07/07/2020",
+    short_sold_date: "May 2020",
+    areaSize: 861,
+    landsize: 570,
+    headline: "Magnificent Estate Metres from the Beach",
+    summaryDescription:
+      "On approximately 800 sqm, this welcoming period home, beautifully maintained and set in mature gardens, offers relaxed family living in a tightly-held precinct close to the Yarra, walking paths and primary and secondary schools.",
+    min_price: 1000000,
+    max_price: 1200000,
+    photos: [
+      {
+        imageType: "Property",
+        advertId: 2016914242,
+        date: "2021-04-21T00:35:22.05Z",
+        fullUrl:
+          "https://bucket-api.domain.com.au/v1/bucket/image/2016914242_1_1_210406_051501-w3901-h2600",
+        rank: 1,
+      },
+    ],
+  },
+];
 
+const CustomCarousel = () => {
   const [activeIndex, setActiveIndex] = React.useState(0);
   const [carouselItems, setCarouselItems] = React.useState(exampleItems);
   const ref = React.useRef(null);
@@ -121,7 +122,7 @@ const CustomCarousel = () => {
   );
 };
 
-export default ({ navigation }) => {
+export default ({navigation, route}) => {
   const welTitle = "Welcome";
   const welContent =
     "We have curated a list of properties that may be of high interest to you. Swipe through the cards to see which one catches your interest and then click to predict the last sold price";
@@ -148,6 +149,7 @@ export default ({ navigation }) => {
         }}
         titleStyle={{ color: AppStyles.color.steedDarkBlue, fontSize: 15 }}
         containerStyle={{ marginTop: 15 }}
+        onPress={() => navigation.navigate('GameDetail')}
       ></Button>
     </ScreenContainer>
   );
---
+++ src/screens/GameResult/index.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import { View, ScrollView, Text, StyleSheet} from 'react-native';
+import { Button } from 'react-native-elements';
+import ScreenContainer from "../../components/ScreenContainer";
+
+import AppStyles from "../../AppStyles";
+
+const styles = StyleSheet.create({
+    scrollview: {
+        flex: 1,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: "red"
+    },
+    container: {
+        width: "100%",
+        // height: "100%",
+        paddingHorizontal: 30,
+        paddingTop: 40,
+        display: 'flex',
+        alignItems: 'center',
+    },
+    header: {
+        marginBottom: 40
+    },
+    accuracy: {
+        display: 'flex',
+        alignItems: 'center', 
+        marginBottom: 25
+    },
+    info: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginBottom: 25
+    },
+    performance: {
+        width: "90%",
+        marginBottom: 60,
+    },
+    stats: {
+        marginBottom: 100
+    },
+    cardContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        // width: "75%",
+    },
+    card: {
+        backgroundColor: AppStyles.color.steedBlue,
+        width: 80,
+        padding: 7,
+        borderRadius: 5
+    },
+    middleCard: {
+        marginHorizontal: 10
+    },
+    button: {
+        width: 160,
+        height: 48,
+        borderWidth: 1
+    }
+})
+
+const textStyle = StyleSheet.create({
+    headerText: {
+        color: AppStyles.color.steedLigthGrey,
+        fontSize: 30,
+    },
+    accuracy: {
+        color: AppStyles.color.steedLigthGrey,
+        fontSize: 16,
+        marginBottom: 3,
+    },
+    accuracyResult: {
+        color: AppStyles.color.steedGreen,
+        fontSize: 50,
+    },
+    price: {
+        color: AppStyles.color.steedGreen,
+        fontSize: 20,
+        marginBottom: 2
+    },
+    soldDate: {
+        color: AppStyles.color.steedDarkGrey,
+        fontSize: 10
+    },
+    performance: {
+        color: AppStyles.color.steedLigthGrey,
+        lineHeight: 18,
+        textAlign: 'center'
+    },
+    stats: {
+        textAlign: 'center',
+        color: AppStyles.color.steedDarkGrey,
+        fontSize: 12,
+        fontStyle: 'italic',
+        marginBottom: 10
+    },
+    cardValue: {
+        fontSize: 16,
+        color: AppStyles.color.steedLigthGrey,
+        marginBottom: 2
+    },
+    indicator: {
+        fontSize: 10,
+        lineHeight: 11,
+        color: AppStyles.color.steedDarkGrey,
+    }
+})
+
+
+export default ({navigation}) => {
+    return (
+        <ScreenContainer style={styles.container}>
+            <View style={styles.header}>
+                <Text style={textStyle.headerText}>Results!</Text>
+            </View>
+            <View style={styles.accuracy}>
+                <Text style={textStyle.accuracy}>Your Accuracy</Text>
+                <Text style={textStyle.accuracyResult}>98.3%</Text>
+            </View>
+            <View style={styles.info}>
+                <View style={{marginRight: 12}}>
+                    <Text style={textStyle.price}>$1.34m</Text>
+                    <Text style={textStyle.soldDate}>House last sold price</Text>
+                    <Text style={textStyle.soldDate}>21-5-2020</Text>
+                </View>
+                <View>
+                    <Text style={textStyle.price}>+230</Text>
+                    <Text style={textStyle.soldDate}>Your Punt in</Text>
+                    <Text style={textStyle.soldDate}>14 secs</Text>
+                </View>
+            </View>
+            <View style={styles.performance}>
+                <Text style={textStyle.performance}>
+                    Setellar performance again Warren. You are one of the top predictors in this category. Kudos!
+                </Text>
+            </View>
+            <View style={styles.stats}>
+                <Text style={textStyle.stats}>Your stats</Text>
+                <View style={styles.cardContainer}>
+                    <View style={styles.card}>
+                        <Text style={textStyle.cardValue}>12000</Text>
+                        <Text style={textStyle.indicator}>Total Points</Text>
+                        <Text style={textStyle.indicator}>Earned</Text>
+                    </View>
+                    <View style={{...styles.card, ...styles.middleCard}}>
+                        <Text style={textStyle.cardValue}>9th</Text>
+                        <Text style={textStyle.indicator}>Leader board</Text>
+                        <Text style={textStyle.indicator}>Position</Text>
+                    </View>
+                    <View style={styles.card}>
+                        <Text style={textStyle.cardValue}>84%</Text>
+                        <Text style={textStyle.indicator}>Average</Text>
+                        <Text style={textStyle.indicator}>Accuracy</Text>
+                    </View>
+                </View>
+            </View>
+            <View>
+                <Button
+                    title="Play Again"
+                    type="outline"
+                    buttonStyle={styles.button}
+                    onPress={() => navigation.navigate('Game')}
+                />
+            </View>
+        </ScreenContainer>
+    )
+}
